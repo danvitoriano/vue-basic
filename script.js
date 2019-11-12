41898cee7c329649e86dfd44aae6e4304c7df391
Danilo Vitoriano <vitoriano08@gmail.com>
2019-11-12 00:23:07 -0300

    vue component

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-var doze = new Vue({ 
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{todo.text}}</li>'
+})
+
+var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
+        lista: [
+            { id: 0, text: 'tomate' },
+            { id: 1, text: 'laranja' },
+            { id: 2, text: 'abacaxi' }
+        ]
     }
 });
